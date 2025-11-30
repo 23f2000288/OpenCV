5b09ce7cde57072758c1524cadba820b8602816b
MD FAIZ KHAN <23f2000288@ds.study.iitm.ac.in>
2025-11-30 15:00:07 +0530

    added

---
+++ Face and Object Detection/face_detect.py
@@ -0,0 +1,30 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    """
+    detectMultiScale() - scan & detect faces
+    1.1 balance, not too slow, not too high
+    minNeighbors = 5
+    """
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+        """
+
+        """
+    cv2.imshow("WebCam face detected", frame)
+
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
